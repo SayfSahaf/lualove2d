52efc20b5c3af444668a4137ba2961deb4b86565
saysah <sayf.sahaf@gmail.com>
2025-08-06 22:27:23 -0400

    fix input:down() function bug

--- game2/main.lua
+++ game2/main.lua
@@ -5,17 +5,19 @@ HyperCircle = require 'objects/hypercircle'
 Input = require 'libraries/input'
 
 function love.load()
-    circle_instance = Circle(400, 300, 50)
-    h_circle_instance = HyperCircle(400, 300, 50, 10, 120)
-
     input = Input()
-    input:bind('mouse1', 'print')
+    --input:bind('pageup', 'print')
+    input:bind('mouse1', 'test')
+    count = 0
 end
 
 function love.update(dt)
-    if input:pressed('print') then print('pressed') end
-    if input:released('print') then print('released') end
-    if input:down('print') then print('down') end
+    if input:pressed('test') then
+        print('left-click')
+        count = count + 1
+        print(count)
+    end
+    if input:down('test', 0.01) then print('mouse down') end
 end
 
 function love.draw()
